696f5da0419abeb843179401fcf230e31a177785
Shapovalov Alexandr <freezko@gmail.com>
2016-01-16 21:17:02 +0300

    Update Server.php

--- src/Server.php
+++ src/Server.php
@@ -13,15 +13,22 @@ class Server {
 	protected $port;
 
 	/**
+	 * @var int
+	 */
+	protected $verbose;
+	
+	/**
 	 *
 	 *
 	 * @param string $host binding host
 	 * @param int $port binding port
+	 * @param int $verbose log
 	 */
-	public function __construct($host, $port)
+	public function __construct($host, $port, $verbose)
 	{
 		$this->host = $host;
 		$this->port = $port;
+		$this->verbose = $verbose;
 	}
 
 	/**
@@ -33,7 +40,7 @@ class Server {
 		$socket = new \React\Socket\Server($loop);
 		$http = new \React\Http\Server($socket, $loop);
 		$http->on('request', function ($request, $response) {
-			with(new HttpSession($this->host, $this->port))->handle($request, $response);
+			with(new HttpSession($this->host, $this->port, $this->verbose))->handle($request, $response);
 		});
 		$socket->listen($this->port, $this->host);
 		$loop->run();
