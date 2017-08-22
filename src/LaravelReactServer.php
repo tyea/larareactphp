c51aa6ebe5c24813e188ec88376e0e1f2b8165df
mnv <mnvx@yandex.ru>
2017-08-22 19:43:40 +0300

    fixed #2 - read body of requests

--- src/LaravelReactServer.php
+++ src/LaravelReactServer.php
@@ -4,6 +4,7 @@ namespace LaravelReactPHP;
 
 use Psr\Http\Message\ServerRequestInterface;
 use React\Http\Response;
+use React\Promise\Promise;
 use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
 use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
 
@@ -59,23 +60,31 @@ class LaravelReactServer
     public function makeRequestCallback()
     {
         return function (ServerRequestInterface $request) {
+            $request->getBody()->on('data', function ($data) use (&$contentLength, &$body) {
+                $body .= $data;
+            });
+            return new Promise(function ($resolve, $reject) use ($request, &$body) {
+                $request->getBody()->on('end', function () use ($resolve, &$contentLength, $request, &$body){
+                    $request2 = $request->withParsedBody(json_decode($body, true));
 
-            $symfonyRequest = with(new HttpFoundationFactory)->createRequest($request);
+                    $symfonyRequest = with(new HttpFoundationFactory)->createRequest($request2);
 
-            $symfonyResponse = app()->handle($symfonyRequest);
+                    $symfonyResponse = app()->handle($symfonyRequest);
 
-            /** @var \Psr\Http\Message\ResponseInterface $psr7response */
-            $psr7response = with(new DiactorosFactory)->createResponse($symfonyResponse);
+                    /** @var \Psr\Http\Message\ResponseInterface $psr7response */
+                    $psr7response = with(new DiactorosFactory)->createResponse($symfonyResponse);
 
-            $response = new Response(
-                $psr7response->getStatusCode(),
-                $psr7response->getHeaders(),
-                $psr7response->getBody(),
-                $psr7response->getProtocolVersion(),
-                $psr7response->getReasonPhrase()
-            );
+                    $response = new Response(
+                        $psr7response->getStatusCode(),
+                        $psr7response->getHeaders(),
+                        $psr7response->getBody(),
+                        $psr7response->getProtocolVersion(),
+                        $psr7response->getReasonPhrase()
+                    );
 
-            return $response;
+                    $resolve($response);
+                });
+            });
         };
     }
 }
